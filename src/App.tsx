c7cce6316e751fbd5326659ee094c906a1372541
Hamlet Ghochikyan <ghochikyanhamlet8@gmail.com>
2025-10-15 01:18:46 +0400

    Added Hello from Hamlet text

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Hello from './Hello';
 
 function App() {
   return (
     <div className="App">
-      
+      <p>Hello from Hamlet</p>
+      <Hello />
     </div>
   );
 }
