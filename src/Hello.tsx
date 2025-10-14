fa9b2217ae7d98f058ec3c95dc1262f5ff417dfd
Hamlet Ghochikyan <ghochikyanhamlet8@gmail.com>
2025-10-14 20:40:10 +0400

    Initial commit with React + TypeScript project

---
+++ src/Hello.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Hello: React.FC = () => {
+  return (
+    <div>
+      <h1>Hello, GitHub & React!</h1>
+      <p>This is my first TSX component.</p>
+    </div>
+  );
+};
+
+export default Hello;
+
+// Սա ավելացնում ենք, որպեսզի TypeScript-ը ճանաչի որպես module
+export {};
